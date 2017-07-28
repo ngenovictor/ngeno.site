bb9d95e83e2b5240c31324b7d5611cbcc67d5d75
Victor Ngeno <ngenovictor321@gmail.com>
2017-07-28 18:47:48 +0300

    create initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ngeno</title>
+	<style>
+		html{height:100%;width:100%;max-width:100%;}
+		body{
+			display:table;
+			height:100%;width:100%;
+			max-width:100%;
+			background-image:url('img/nairobi.jpg');
+			background-size:cover;
+		}
+		div{
+			display:table-cell;height:100%;width:100%;vertical-align:middle;text-align:center;
+		}
+	</style>
+
+</head>
+<body>
+	<div>Coming Soon</div>
+</body>
+</html>
