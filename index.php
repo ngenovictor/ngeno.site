2bb854cda8616724677703440ca46158bfc77a76
Victor Ngeno <ngenovictor321@gmail.com>
2017-07-28 19:32:20 +0300

    working landing page

--- index.php
+++ index.php
@@ -1,23 +1,67 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="portfolio">
+  <meta name="author" content="Victor Ngeno">
 	<title>Ngeno</title>
-	<style>
-		html{height:100%;width:100%;max-width:100%;}
-		body{
-			display:table;
-			height:100%;width:100%;
-			max-width:100%;
-			background-image:url('img/nairobi.jpg');
-			background-size:cover;
-		}
-		div{
-			display:table-cell;height:100%;width:100%;vertical-align:middle;text-align:center;
-		}
-	</style>
-
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
-	<div>Coming Soon</div>
+	<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header page-scroll">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+				</button>
+				<a class="navbar-brand" href="#page-top">Ng'eno</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="hidden"><a href="#page-top"></a></li>
+					<li class="page-scroll"><a href="#portfolio">Portfolio</a></li>
+					<li class="page-scroll"><a href="#about">About</a></li>
+					<li class="page-scroll"><a href="#contact">Contact</a></li>
+					<li class="page-scroll"><a href="blog.php">Blog</a></li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container-fluid -->
+  </nav>
+
+  <!-- Header -->
+	<header>
+		<div class="container" id="maincontent" tabindex="-1">
+			<div class="row">
+				<div class="col-lg-12">
+					<img class="img-responsive" src="img/victor.png" alt="">
+					<div class="intro-text">
+						<h1 class="name">VICTOR NG'ENO</h1>
+						<hr class="star-light">
+						<span class="skills">GIS and Mapping  - Web and Mobile Developer</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+
+
+
+
+
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
 </body>
 </html>
